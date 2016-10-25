bd9ae2d05e1f3e6f344daf38af9540cbf9c43b97
Aakash Patel <aakashpatel02031993@gmail.com>
2016-10-25 12:40:15 -0400

    Initial commit

---
+++ js/loginvalidation.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+function Validation(){
+		var email = document.getElementById('email').value;
+		var password = document.getElementById('pwd').value;
+		
+		if (email==="" || password==="") {
+			alert('please enter required fields');
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
